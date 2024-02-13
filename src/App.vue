cc2a1a3b82aa38f9fa1afa904d74ae3a56ea5067
lukxnut <kristian2015ok@gmail.com>
2024-02-13 19:50:13 +0700

    Create search button and give style of it

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,35 @@
 <template>
   <header>
     <h1>TodoList</h1>
+    <button class="search-btn"></button>
   </header>
 
   <router-view />
 </template>
 
-<style></style>
+<script>
+  export default {
+  }
+</script>
 
 <style>
 header {
-  background: #5F30E2;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background: #5f30e2;
   padding: 16px;
   color: white;
+}
+
+.search-btn {
+  width: 30px;
+  height: 30px;
+
+  background: url(./assets/img/search-outline.svg);
+  background-size: contain;
+  margin-left: 16px;
+  cursor: pointer;
+  border: none;
 }
 </style>
