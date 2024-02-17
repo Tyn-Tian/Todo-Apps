71518d2029f43e015d96420a4ce7cdd4417242dd
lukxnut <kristian2015ok@gmail.com>
2024-02-17 12:21:16 +0700

    Delete router feature because this project doesn't use router

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,120 @@
     <button class="search-btn" @click="searchBtn"></button>
   </header>
 
-  <router-view />
+  <div class="wrapper">
+    <Search v-if="this.$store.state.isSearch" />
+
+    <Form :bool="this.$store.state.isEdit" :todoID="this.todoID" />
+
+    <div v-if="!this.$store.state.isSearch">
+      <TodoList>
+        <h2 class="container-header">Yang harus dilakukan</h2>
+        <div class="list-item" id="todos">
+          <div class="item shadow" v-for="todo in todos" :id="todo.id">
+            <div class="inner">
+              <h2>{{ todo.task }}</h2>
+              <p>{{ todo.timestamp }}</p>
+            </div>
+            <button class="edit-button" @click="editBtn(todo.id)"></button>
+            <button class="check-button" @click="checkBtn(todo.id)"></button>
+          </div>
+        </div>
+      </TodoList>
+
+      <TodoList>
+        <h2 class="container-header">Yang sudah dilakukan</h2>
+        <div class="list-item" id="completed-todos">
+          <div
+            class="item shadow"
+            v-for="completeTodo in completeTodos"
+            :id="completeTodo.id"
+          >
+            <div class="inner">
+              <h2>{{ completeTodo.task }}</h2>
+              <p>{{ completeTodo.timestamp }}</p>
+            </div>
+            <button
+              class="undo-button"
+              @click="undoBtn(completeTodo.id)"
+            ></button>
+            <button
+              class="trash-button"
+              @click="removeBtn(completeTodo.id)"
+            ></button>
+          </div>
+        </div>
+      </TodoList>
+    </div>
+  </div>
 </template>
 
 <script>
-  export default {
-    methods: {
-      searchBtn() {
-        this.$store.commit("changeSearchBool")
-      }
+import Form from "./components/Form.vue";
+import TodoList from "./components/TodoList.vue";
+import Search from "./components/Search.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    Form,
+    TodoList,
+    Search,
+  },
+  data() {
+    return {
+      todoID: null,
+    };
+  },
+  computed: {
+    todos() {
+      return this.$store.getters.uncompletedTodos;
+    },
+    completeTodos() {
+      return this.$store.getters.completedTodos;
+    },
+  },
+  methods: {
+    searchBtn() {
+      this.$store.commit("changeSearchBool")
+    },
+    checkBtn(todoID) {
+      const todoTarget = this.findTodo(todoID);
+      this.$store.commit("addTaskToCompleted", todoTarget);
+      this.saveData();
+    },
+    editBtn(todoID) {
+      this.$store.commit("changeEditBool");
+      this.todoID = todoID;
+    },
+    undoBtn(todoID) {
+      const todoTarget = this.findTodo(todoID);
+      this.$store.commit("undoTaskFromCompleted", todoTarget);
+      this.saveData();
+    },
+    removeBtn(todoID) {
+      const todoTarget = this.findTodoIndex(todoID);
+      this.$store.commit("removeTaskFromCompleted", todoTarget);
+      this.saveData();
+    },
+    findTodo(todoID) {
+      return this.$store.getters.findTodo(todoID);
+    },
+    findTodoIndex(todoID) {
+      return this.$store.getters.findTodoIndex(todoID);
+    },
+    saveData() {
+      this.$store.getters.saveData;
+    },
+    loadDataFromStorage() {
+      this.$store.getters.loadDataFromStorage;
+    },
+  },
+  beforeMount() {
+    if (this.$store.getters.isStorageExits) {
+      this.loadDataFromStorage();
     }
-  }
+  },
+};
 </script>
 
 <style>
@@ -36,5 +139,102 @@ header {
   margin-left: 16px;
   cursor: pointer;
   border: none;
+}
+
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.item {
+  border-radius: 16px;
+  margin-top: 16px;
+  padding: 12px 24px 12px 24px;
+  background: white;
+  display: flex;
+  align-items: center;
+}
+
+.item > .inner h2 {
+  margin-bottom: unset;
+}
+
+.item > .inner p {
+  margin-top: 5px;
+}
+
+.check-button {
+  width: 40px;
+  height: 40px;
+
+  background: url("./assets/img/check-outline.svg");
+  background-size: contain;
+  margin-left: 16px;
+  cursor: pointer;
+  border: none;
+}
+
+.check-button:hover {
+  background: url("./assets/img/check-solid.svg");
+  background-size: contain;
+}
+
+.edit-button {
+  width: 40px;
+  height: 40px;
+
+  background: url("./assets/img/edit-solid.svg");
+  background-size: contain;
+  margin-left: auto;
+  cursor: pointer;
+  border: none;
+}
+
+.trash-button {
+  width: 40px;
+  height: 40px;
+
+  background: url("./assets/img/trash-outline.svg");
+  background-size: contain;
+  margin-left: 16px;
+  cursor: pointer;
+  border: none;
+}
+
+.trash-button:hover {
+  background: url("./assets/img/trash-fill.svg");
+  background-size: contain;
+}
+
+.undo-button {
+  width: 40px;
+  height: 40px;
+
+  background: url("./assets/img/undo-ouline.svg");
+  background-size: contain;
+  margin-left: auto;
+  cursor: pointer;
+  border: none;
+}
+
+.undo-button:hover {
+  background: url("./assets/img/undo-ouline.svg");
+  background-size: contain;
+}
+
+.check-button:focus,
+.trash-button {
+  outline: none;
+}
+
+@media only screen and (max-width: 1024px) {
+  .wrapper {
+    margin: 0;
+  }
+
+  .list-item {
+    width: 100%;
+  }
 }
 </style>
